5d85cb89f0954e183b656e15c315804bc9d41c6d
WafiWadud <wafinashwan@gmail.com>
2024-08-02 20:28:53 +0600

    Added a 404 screen

--- pypandoc_server.py
+++ pypandoc_server.py
@@ -14,13 +14,16 @@ app: FastAPI = FastAPI()
 
 @app.get("/{filename:path}")
 async def serve_file(filename: str):
-    if filename.endswith(".md"):
-        return HTMLResponse(
-            content='<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js" integrity="sha512-D9gUyxqja7hBtkWpPWGt9wfbfaMGVt9gnyCvYa+jojwwPHLCzUm5i8rpk7vD7wNee9bA35eYIjobYPaQuKS1MQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/darcula.css" integrity="sha512-LBTd9wWU9cpQj0FeE4eAN9aZYIp7zDFIRc5yyGyRaVaob8vkmfrJIAxg2lpZppwzAZMA5ErYbpngxxGGMKpRZA==" crossorigin="anonymous" referrerpolicy="no-referrer" /><link rel="stylesheet" href="index.css"><script>hljs.highlightAll();</script>'
-            + convert_file(filename, to="html", format="md")
-        )
-    else:
-        return FileResponse(path=filename)
+    try:
+        if filename.endswith(".md"):
+            return HTMLResponse(
+                content='<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js" integrity="sha512-D9gUyxqja7hBtkWpPWGt9wfbfaMGVt9gnyCvYa+jojwwPHLCzUm5i8rpk7vD7wNee9bA35eYIjobYPaQuKS1MQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/base16/darcula.css" integrity="sha512-LBTd9wWU9cpQj0FeE4eAN9aZYIp7zDFIRc5yyGyRaVaob8vkmfrJIAxg2lpZppwzAZMA5ErYbpngxxGGMKpRZA==" crossorigin="anonymous" referrerpolicy="no-referrer" /><link rel="stylesheet" href="index.css"><script>hljs.highlightAll();</script>'
+                + convert_file(filename, to="html", format="md")
+            )
+        else:
+            return FileResponse(path=filename)
+    except RuntimeError:
+        return FileResponse(path="404.html")
 
 
 run(app=app, host="0.0.0.0", port=80, loop="uvloop")
